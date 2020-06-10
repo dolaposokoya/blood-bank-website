320fda77f135ea9af7be67ef359d39393f7ca06c
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2020-06-10 15:36:08 +0530

    Add files via upload

--- script/getUser.js
+++ script/getUser.js
@@ -1,31 +1,44 @@
 const url = "https://api-bloodbank.herokuapp.com/user/get-all-user";
 // const url = "http://localhost:5000/user/get-all-user";
 let token = localStorage.getItem("userToken");
+document.querySelector(".back").classList.add("backPop");
+document.querySelector(".main").classList.add("spinner3");
 
-fetch(url, {
-  method: "GET",
-  headers: {
-    authorization: "Bearer " + token,
-    "content-type": "application/json",
-    accept: "application/json",
-  },
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let users = data.data;
-    users.forEach((item) => {
-      const list = document.querySelector("#user-list");
-
-      const row = document.createElement("tr");
-      row.innerHTML = `<td>${item.first_name}</td>
-      <td>${item.last_name}</td>
-      <td>${item.email}</td>
+function checkToken() {
+  if (localStorage.getItem("userToken")) {
+    fetch(url, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json",
+        accept: "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let users = data.data;
+        document.querySelector(".main").classList.remove("spinner3");
+        document.querySelector(".back").classList.remove("backPop");
+        users.forEach((item, index) => {
+          const list = document.querySelector("#user-list");
+          const row = document.createElement("tr");
+          row.innerHTML = `<td><i class="fas fa-user"></i> ${item.first_name} ${item.last_name}</td>
+      <td><i class="fas fa-envelope"></i> ${item.email}</td>
+      <td><i class="fas fa-tint"></i> ${item.blood_group}</td>
       <td><a href="#"  class="btn btn-primary btn-sm" id="tableContact" onClick="openModal()">contact</a></td>`;
-      list.appendChild(row);
-    });
-  });
+          list.appendChild(row);
+        });
+        document.querySelector(".main").classList.remove("spinner3");
+        document.querySelector(".back").classList.remove("backPop");
+      });
+  } else {
+    window.location.assign("/index.html");
+  }
+}
+
+checkToken();
 
 var modal = document.getElementById("simpleModal");
 var openBtn = document.getElementById("tableContact");
--- script/loginForm.js
+++ script/loginForm.js
@@ -1,18 +1,46 @@
 // const url = "http://localhost:5000/user/login-user";
 const url = "https://api-bloodbank.herokuapp.com/user/login-user";
+let token = localStorage.getItem("userToken");
+function showAlert(message, className) {
+  const div = document.createElement("div");
+  div.className = `alert alert-${className}`;
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const main = document.querySelector("main");
+  container.insertBefore(div, main);
 
+  // Vanish in 3 seconds
+  setTimeout(() => document.querySelector(".alert").remove(), 4000);
+}
+
+function checkToken() {
+  if (localStorage.getItem("userToken")) {
+    console.log("Token", token);
+    window.location.assign("../views/pages/about.html");
+  } else {
+    submitForm();
+  }
+}
+
+checkToken();
 function submitForm() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
+  document.querySelector(".back").classList.add("backPop");
+  document.querySelector(".main").classList.add("spinner3");
   let formData = {
     email: email,
     password: password,
   };
   let data = JSON.stringify(formData);
   if (formData.email == "") {
-    alert("Email is empty");
+    document.querySelector(".main").classList.remove("spinner3");
+    document.querySelector(".back").classList.remove("backPop");
+    showAlert("Email is empty", "warning");
   } else if (formData.password == "") {
-    alert("Password is empty");
+    document.querySelector(".main").classList.remove("spinner3");
+    document.querySelector(".back").classList.remove("backPop");
+    showAlert("Password is empty", "warning");
   } else {
     fetch(url, {
       method: "POST",
@@ -27,9 +55,14 @@ function submitForm() {
       })
       .then((data) => {
         if (data.success == false) {
-          alert(data.message);
+          document.querySelector(".main").classList.remove("spinner3");
+          document.querySelector(".back").classList.remove("backPop");
+          showAlert(data.message, "warning");
+          // alert(data.message);
         } else if (data.success == true) {
-          alert(data.message);
+          showAlert(data.message, "success");
+          document.querySelector(".main").classList.remove("spinner3");
+          document.querySelector(".back").classList.remove("backPop");
           localStorage.setItem("userToken", data.token);
           localStorage.setItem("profileId", data.profile_id);
           window.location.assign("../views/pages/about.html");
