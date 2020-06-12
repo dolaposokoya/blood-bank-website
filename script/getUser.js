963f02ee19cae5a4f497b8fe39070c826d99de1c
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2020-06-12 16:51:50 +0530

    Add files via upload

--- script/getUser.js
+++ script/getUser.js
@@ -1,5 +1,5 @@
-const url = "https://api-bloodbank.herokuapp.com/user/get-all-user";
-// const url = "http://localhost:5000/user/get-all-user";
+// const url = "https://api-bloodbank.herokuapp.com/user/get-all-user";
+const url = "http://localhost:5000/user/get-all-user";
 let token = localStorage.getItem("userToken");
 document.querySelector(".back").classList.add("backPop");
 document.querySelector(".main").classList.add("spinner3");
@@ -21,19 +21,27 @@ function checkToken() {
         let users = data.data;
         document.querySelector(".main").classList.remove("spinner3");
         document.querySelector(".back").classList.remove("backPop");
-
         users.forEach((item, index) => {
-          const list = document.querySelector(".conatiner");
-          const row = document.createElement("div");
-          row.setAttribute("class", "box");
-          row.innerHTML = `<p><i class="fas fa-user"></i> ${item.first_name} ${item.last_name}</p>
-          <hr>
-      <p><i class="fas fa-envelope"></i> ${item.email}</p> <hr>
-      <p><i class="fas fa-tint"></i> ${item.blood_group}</p> <hr>
-      <p><i class="fa fa-map-marker"></i> ${item.city}</p> <img class="saveImage" src="../images/banner.png" alt="image" style="float: right"/> <hr>
-      <a class="btn btn-primary btn-sm" style="cursor: pointer" id="tableContact" onClick="openModal()">contact</a>`;
+          const list = document.querySelector("#user-list");
+          const row = document.createElement("tr");
+          row.innerHTML = `<td data-label="Name"><i class="fas fa-user"></i> ${item.first_name}</td>
+      <td data-label="Email"> <i class="fas fa-venus-mars"></i> ${item.gender}</td>
+      <td data-label="Blood Group"><i class="fas fa-tint"></i> ${item.blood_group}</td><td data-label="City"><i class="fas fa-map-marker"></i> ${item.city}</td><td data-label="Action"><button class="btn btn-primary btn-sm"  onClick="openModal()">contact</button></td>`;
           list.appendChild(row);
         });
+
+        //   users.forEach((item, index) => {
+        //     const list = document.querySelector(".conatiner");
+        //     const row = document.createElement("div");
+        //     row.setAttribute("class", "box");
+        //     row.innerHTML = `<p><i class="fas fa-user"></i> ${item.first_name} ${item.last_name}</p>
+        //     <hr>
+        // <p><i class="fas fa-envelope"></i> ${item.email}</p> <hr>
+        // <p><i class="fas fa-tint"></i> ${item.blood_group}</p> <hr>
+        // <p><i class="fa fa-map-marker"></i> ${item.city}</p> <img class="saveImage" src="../images/banner.png" alt="image" style="float: right"/> <hr>
+        // <button class="btn btn-primary btn-sm" style="cursor: pointer" id="tableContact" onClick="openModal()">contact</button>`;
+        //     list.appendChild(row);
+        //   });
         document.querySelector(".main").classList.remove("spinner3");
         document.querySelector(".back").classList.remove("backPop");
       });
